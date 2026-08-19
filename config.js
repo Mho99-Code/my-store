a5b0b2a6edea6f71acf946210fc3651b9b4fa6e5
Mho99-Code <mh9844532@gmail.com>
2026-08-19 14:13:55 +0300

    Update APPS_SCRIPT_URL to new endpoint

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbw-UHmCmFqNg7FCeWSTNWfPBs193Buky5DKPJbdSlsczYVDiIo4FCrXw1nH5-zv6Qgktg/exec";
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxy4UT3UM3T5qtHzWt0DEVK2VBVUH_K_PHOsZ8VOMqF2pN3phFe8wIXsEJONbhOW741gw/exec";
